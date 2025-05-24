b42faf50fc68115e77e0b5b95c00de6759bd9d7d
SpaceScriptSushil <sushil@scriptspace.org>
2025-05-24 13:28:28 +0530

    deployed

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,8 @@ export default function HomePage() {
     <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center px-4">
       <h1 className="text-4xl font-bold text-green-600 mb-4">🎉 Congrats!</h1>
       <p className="text-lg text-gray-700">
-        You have successfully deployed the app.
+        You have successfully deployed the app. Finally done By using CI/CD
+        Pipeline
       </p>
     </main>
   );
